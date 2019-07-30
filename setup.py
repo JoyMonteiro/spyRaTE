24008e19aed96c976d2c17b36b7f948a873b2997
Joy M. Monteiro <joy.merwin@gmail.com>
2019-07-30 11:40:04 +0200

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/JoyMonteiro/spyrate',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
     cmdclass={
         'build_ext': SpyrateBuildExt,
