b943aee2cd2afdb13b6703c8f2ea8ff1b8b8b203
Joy M. Monteiro <joy.merwin@gmail.com>
2019-07-30 12:11:38 +0200

    Bump version: 0.1.4 → 0.1.5

--- spyrate/__init__.py
+++ spyrate/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Joy Merwin Monteiro"""
 __email__ = 'joy.merwin@gmail.com'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 from .rte.rte_interface import CompiledRTEInterface, UserCompiledRTEInterface
 from .spyrate import Spyrate
